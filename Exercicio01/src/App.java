91b333e61b883ddc5bddcd5a0dbc234110bbddeb
Letícia Clasen <14635057-letsquicia@users.noreply.gitlab.com>
2023-05-30 20:15:55 +0000

    Update file App.java

--- Exercicio01/src/App.java
+++ Exercicio01/src/App.java
@@ -1,5 +1,23 @@
+import java.util.Date;
+import java.util.Locale;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Date relogio = new Date();
+        System.out.println("A hora do sistema é: ");
+        System.out.println(relogio.toString());
+
+        Locale loc = Locale.getDefault();
+        System.out.println("Seu idioma é: ");
+        System.out.println(loc.getDisplayLanguage(loc));
+
+        /*
+        yhToolkit t = Toolkit.getDefaultToolkit();
+        Dimension d = t.getScreenSize();
+        System.out.println( "A resolução do seu sistema é: " + d.width +" X " + d.height);    
+        */
+    
+        System.out.println("bah");
     }
 }
